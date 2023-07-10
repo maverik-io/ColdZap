c5692b9b1d9d0a235346cade57b977cb4d81610d
Joseph Chacko <112103666+CrystallineXXII@users.noreply.github.com>
2023-07-10 23:11:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ while not quit:
 	quit = pg.event.get(pg.QUIT)
 	screen.fill('#4e9c16')
 	
+	for x,y in [(i,j) for i in range(7) for j in range(9) if (i+j)%2]:
+		pg.draw.rect(screen,'#6acf21',(100*x,100*y,100,100))
+	
 	pg.display.flip()
 	
 pg.quit()
